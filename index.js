7ca1d804bd2daec03a70ffbcba4e023e8067d787
alexnorrell <norrell.alex@gmail.com>
2023-03-07 16:01:24 -0500

    populating cities on left of page

--- index.js
+++ index.js
@@ -11,18 +11,25 @@ searchBtn.addEventListener('click', function () {
 })
 
 function getCityGeoLocation(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
         .then((responce) => responce.json())
         .then((data) => {
-            console.log(data.city.coord)
-            getWeather(data.city.coord)
+            let today = {
+                date: data.list[0].dt_txt.split(' ')[0].split('-').join('/'), 
+                temp: data.list[0].main.temp,
+                humidity: data.list[0].main.humidity,
+                wind: data.list[0].wind.speed,
+                icon: data.list[0].weather[0].icon
+            }
+             getWeather(data.city.coord, today, city)
+          
         })
         .catch((error) => console.log(error))
 
 }
 
-function getWeather(coord) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}`)
+function getWeather(coord, today, city) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=imperial`)
         .then((responce) => responce.json())
         .then((data) => {
             const nextFive = []
@@ -36,9 +43,63 @@ function getWeather(coord) {
                     icon: item.weather[0].icon
                 })
             }
-            console.log(nextFive)
+            updatePage({city, today, nextFive})
+            saveToLocalStorage({city, today, nextFive})
+            populateCities()
         })
         .catch((error) => console.log(error))
 }
 
+function updatePage(data){
+    console.log(data)
+    let todayContainer = document.querySelector('.today')
+    let todayContent = `
+                <h2>${data.city} (${data.today.date}) <img src="https://openweathermap.org/img/w/${data.today.icon}.png"></h2>
+                <p>Temp: ${data.today.temp} &deg;F</p>
+                <p>Wind: ${data.today.wind} MPH</p>
+                <p>Humidity: ${data.today.humidity}%</p>
+    `
+    todayContainer.innerHTML = todayContent;
+    const nextFiveContainer = document.querySelector('.items-container')
+    let nextFiveContent = ''
+    for (const day of data.nextFive) {
+        let div= `         
+    <div>
+        <h3>${day.date}</h3>
+        <h4><img src="https://openweathermap.org/img/w/${day.icon}.png"></h4>
+        <p>Temp: ${day.temp} &deg;F</p>
+        <p>Wind: ${day.wind} MPH</p>
+        <p>Humidity: ${day.humidity}%</p>
+    </div>`
+    nextFiveContent += div
+    }
+    nextFiveContainer.innerHTML=nextFiveContent;
+}
 
+function saveToLocalStorage(data){
+    let db = JSON.parse(localStorage.getItem('cities_db')) ||[]
+    db.push(data)
+    localStorage.setItem('cities_db', JSON.stringify(db))
+}
+
+function readLocalStorage(){
+    let db = JSON.parse(localStorage.getItem('cities_db'))
+    return db
+}
+
+function populateCities(){
+    let citiesContainer = document.querySelector('.left-container ul')
+    let dbData = readLocalStorage()
+    citiesContainer.innerHTML =''
+    dbData.forEach(el => {
+        let li = document.createElement('li')
+        li.textContent =el.city
+        li.addEventListener('click', function(event){
+            var city= event.target.textContent
+            getCityGeoLocation(city)
+        })
+        citiesContainer.appendChild(li)
+    });
+}
+
+populateCities()
